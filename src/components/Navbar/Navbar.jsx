b1bc904f892c5e6cb851b6920e30df675436d499
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2022-04-22 14:53:20 +0530

    day 1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import './Navbar.css';
 const Navbar = () => (
   <nav className='app__navbar'>
     <div className='app__navbar-logo'>
-      <img src={images.gericht} alt="app logo" />
+      <img src={images.gericht} alt="app__logo" />
     </div>
     <ul className='app__navbar-links'>
       <li className='p__opensans'><a href="#home">Home</a></li>
@@ -17,9 +17,23 @@ const Navbar = () => (
       <li className='p__opensans'><a href="#contact">Contact</a></li>
     </ul>
     <div className='app__navbar-login'>
-      <a href="#login" className='p__opensans'>Log In / Register</a>
+      <a href="#login" className='p__opensans'>Log In / Registration</a>
       <div />
       <a href="/" className='p__opensans'>Book Table</a>
+    </div>
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu color='#fff' fontSize={27} onClick={() => { }} />
+      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className='overlay__close' onClick={()=>{}} />
+        <ul className='app__navbar-smallscreen-links'>
+          <li className='p__opensans'><a href="#home">Home</a></li>
+          <li className='p__opensans'><a href="#about">About</a></li>
+          <li className='p__opensans'><a href="#menu">Menu</a></li>
+          <li className='p__opensans'><a href="#awards">Awards</a></li>
+          <li className='p__opensans'><a href="#contact">Contact</a></li>
+        </ul>
+      </div>
+
     </div>
   </nav>
 );
