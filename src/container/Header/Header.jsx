9904bbd8b08d7d8c6566aa6dee8a68f40ec8126c
gaurav3278 <gaurav.rai2020@vitbhopal.ac.in>
2022-04-28 12:48:10 +0530

    28th april

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SubHeading } from ',,/,,/components';
+import { SubHeading } from '../../components';
 
 import { images } from '../../constants';
 import './Header.css';
@@ -7,10 +7,13 @@ import './Header.css';
 const Header = () => (
   <div className='app__header app__wrapper section__padding' id = 'home'>
     <div className='app__wrapper_info'>
-      <SubHeading />
+      <SubHeading title = 'Chase the new flavour' />
+      <h1 className='app__header-h1'>The Key to Fine Dining</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum tincidunt semper pulvinar. Pellentesque pharetra augue lorem. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+      <button type='button' className='custom__button'>Explore Menu </button>
     </div>
-    <div className='app__wrapper_image'>
-
+    <div className='app__wrapper_img'>
+      <img src={images.welcome} alt="header img" />
     </div>
   </div>
 );
